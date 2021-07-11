8aec3272ca0e78c1fdee747359f74888754c3673
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-07-12 00:16:38 +0100

    Move image update scripts to directory

---
+++ image_updater/image_updater_local.sh
@@ -0,0 +1,30 @@
+DOCKER_IMAGE="theobrown/csgo-server:latest"
+CSGO_DIR="/home/user/csgo-server"
+
+CSGO_VERSION=$(docker inspect --format '{{ index .Config.Labels "csgo_version" }}' $DOCKER_IMAGE)
+
+if [ -z $CSGO_VERSION ]
+then
+    CSGO_VERSION=0
+fi
+
+echo "Current CS:GO version: $CSGO_VERSION"
+
+# Steam API provides a URL that can be used to check whether a specific version of a game is up to date
+JSON=$(curl -s "http://api.steampowered.com/ISteamApps/UpToDateCheck/v1?appid=730&version=$CSGO_VERSION" | jq .response)
+
+## If the CSGO install is not up to date, then update the docker image
+if ! $(echo $JSON | jq .up_to_date)
+then
+    LATEST_VERSION=$(echo $JSON | jq .required_version)
+    echo "Latest CS:GO version: $LATEST_VERSION"
+    echo "Launching container..."
+    docker run --name=csgo_update_container -it $DOCKER_IMAGE bash /home/user/server_update.sh
+    echo "Committing changes..."
+    docker commit --change "LABEL csgo_version=$LATEST_VERSION" csgo_update_container $DOCKER_IMAGE
+    echo "Deleting container..."
+    docker container rm csgo_update_container
+fi
+
+echo "CS:GO is up to date."
+
